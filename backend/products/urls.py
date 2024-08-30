5ed8ce44729223a7519c3341df011bb2da3ec55b
Gabriel Costa Lucas <gcostaluccas95@gmail.com>
2024-08-30 20:31:03 -0300

    api

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path 
+# product urls
+
+from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("<int:pk>/" , views.ProducDetailApiView.as_view())#localhost:8000/api
+    # API read per pk localhost:8000/api/product
+    path("<int:pk>/", views.ProducDetailApiView.as_view()),
+    # api list products # localhost:8000/api/product/list
+    path("list/", views.ProducListApiView.as_view()),
+    # api create product # localhost:8000/api/product/create
+    path("create/", views.ProducCreateApiView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -6,6 +6,19 @@ from .models import Product
 from .serializers import SerializerProduct
 
 
+# generic API create an Product
+class ProducCreateApiView(generics.CreateAPIView):
+  queryset = Product.objects.all()
+  serializer_class = SerializerProduct
+  
+  
+# generic API
+class ProducListApiView(generics.ListAPIView):
+  queryset = Product.objects.all()
+  serializer_class = SerializerProduct
+
+
+# generic API read an Product
 class ProducDetailApiView(generics.RetrieveAPIView):
   queryset = Product.objects.all()
   serializer_class = SerializerProduct
