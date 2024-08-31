d3309c55cb7bdbada0a49109c464184a00e0c9f3
Gabriel Costa Lucas <gcostaluccas95@gmail.com>
2024-08-31 14:48:27 -0300

    CRUD

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -6,6 +6,10 @@ from . import views
 urlpatterns = [
     # API read per pk localhost:8000/api/product
     path("<int:pk>/", views.ProducDetailApiView.as_view()),
+    # API update per pk localhost:8000/api/product/update
+    path("update/<int:pk>/", views.ProductUpdateApiView.as_view()),
+    # API delete per pk localhost:8000/api/product/delete
+    path("delete/<int:pk>/", views.ProductDeleteApiView.as_view()),
     # api list products # localhost:8000/api/product/list
     path("list/", views.ProducListApiView.as_view()),
     # api create product # localhost:8000/api/product/create
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,6 @@
-#products app views 
+# products app views
 
-from rest_framework import generics
+from rest_framework import generics , permissions , authentication
 
 from .models import Product
 from .serializers import SerializerProduct
@@ -8,17 +8,37 @@ from .serializers import SerializerProduct
 
 # generic API create an Product
 class ProducCreateApiView(generics.CreateAPIView):
-  queryset = Product.objects.all()
-  serializer_class = SerializerProduct
-  
-  
-# generic API
+    queryset = Product.objects.all()
+    serializer_class = SerializerProduct
+
+
+# generic API delete an Product
+class ProductDeleteApiView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = SerializerProduct
+    lookup_field = "pk"
+    
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
+
+# generic API update an Product
+class ProductUpdateApiView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = SerializerProduct
+    lookup_field = "pk"
+    
+    def perform_update(self, serializer):
+      instance = serializer.save()
+
+
+# generic list API
 class ProducListApiView(generics.ListAPIView):
-  queryset = Product.objects.all()
-  serializer_class = SerializerProduct
+    queryset = Product.objects.all()
+    serializer_class = SerializerProduct
 
 
 # generic API read an Product
 class ProducDetailApiView(generics.RetrieveAPIView):
-  queryset = Product.objects.all()
-  serializer_class = SerializerProduct
+    queryset = Product.objects.all()
+    serializer_class = SerializerProduct
